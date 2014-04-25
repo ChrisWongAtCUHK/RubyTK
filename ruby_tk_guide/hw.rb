892da8a4b5affbafe7a735c180ab9291f4bfdfb1
Chris Wong <wc38.wong@gmail.com>
2014-04-25 17:04:43 +0800

    set root width & height

--- ruby_tk_guide/hw.rb
+++ ruby_tk_guide/hw.rb
@@ -1,9 +1,13 @@
 require 'tk'
 
-root = TkRoot.new { title "Ruby 1.8.7 support!" }
+root = TkRoot.new {
+			title "Ruby 1.8.7 support!"
+			padx 100
+			padx 50
+		}
 TkLabel.new(root) do
    text 'Chris Wong is handsome!'
-   pack { width 500; padx 15 ; pady 15; side 'left' }
+   pack { padx 15 ; pady 15; side 'left' }
 end
 Tk.mainloop
 
